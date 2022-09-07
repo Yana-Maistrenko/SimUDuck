458c7cc08c01ed270be650a059480e6021758b72
Yana-Maistrenko <84749583+Yana-Maistrenko@users.noreply.github.com>
2022-09-07 11:33:58 +0300

    Add files via upload

---
+++ Duck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimUDuck
+{
+   class Duck
+    {
+        public bool c_swim;
+        public string c_display;
+        public string[] ducks;
+        public Duck(bool swim, string display)
+        {
+            c_swim = swim;
+            c_display = display;
+        }
+
+        private static bool Get_swim()
+        {
+            return swim();
+        }
+
+        public FlyBehavior flyBehavior;
+        public QuackBehavior quackBehavior;
+
+        //public static Duck AddNewDuck()
+        //{
+            //var ducks = new Duck();
+            //ducks.Push();
+        //}
+
+        public static T[] Append<T>(this T[] array, T item)
+        {
+            List<T> ducks = new List<T>(array);
+            ducks.Add(item);
+            return ducks.ToArray();
+        }
+        public string Display()
+        {
+            return ("I'm a real ");
+        }
+        public static bool swim()
+        {
+            //return 
+        }
+    }
+}
---
+++ MallardDuck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimUDuck
+{
+    class MallardDuck : Duck
+    {
+        public void Display() {
+            Console.WriteLine(
+    }
+}
